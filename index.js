7108c673a19f24a70843e03907592351972c591f
danschumann <dan.schumann@gmail.com>
2014-08-29 08:19:26 -0500

    in the middle of adding manifests

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
 var
   _             = require('underscore'),
-  fs            = require('fs'),
+  when          = require('when'),
+  packageInfo   = require('./package.json'),
+  fs            = require('final-fs'),
   child         = require('child_process'),
   CoffeeScript  = require('coffee-script'),
   File          = require('./lib/file'),
@@ -10,13 +13,11 @@ require('colors');
 
 // Recursive directory scraping / loading / callbacks / watching / callbacks
 //   options to compile
-module.exports = function(options) {
+var loaddir = function(options) {
 
   var directory;
 
-  options = options || {};
-
-  if (options.debug) console.log('Loaddir debug mode!'.zebra);
+  options = options ? _.clone(options) : {};
 
   if (options.recursive == null)
     options.recursive = true;
@@ -35,8 +36,40 @@ module.exports = function(options) {
   // This has all of the files added to it
   options.output = {};
 
-  // returns Promise
-  return (new Directory(options)).process();
+  return when().then(function(){
+    if (options.manifest)
+      return fs.readFile(options.manifest)
+  }).then(function(manifest){
+
+    if (manifest) {
+      manifest = JSON.parse(manifest)
+      console.log('MANFIFESTOTOTOOTTOTO'.zebra, manifest);
+    }
+    options.manifest = manifest;
+
+    // returns Promise
+    var dir = new Directory(options)
+    return dir.load().then(function() {
+      // Top directory doesn't get stats loaded
+      return fs.lstat(dir.path);
+    }).then(function(stats){
+      dir.stats = stats
+
+      // Build manifest file
+      if (options.manifest) {
+
+        if (options.require) throw new Error('Cannot build a manifest for directly required files');
+
+        console.log(dir.buildManifest());
+        fs.writeFile(options.manifest, JSON.stringify(dir.buildManifest(), null, '  '));
+
+      }
+
+      return options.output;
+    }).otherwise(function(er){
+      console.log("dohh?".red, (er + '').red, er.stack);
+    });
+  });
 
 };
 
@@ -44,4 +77,8 @@ module.exports.File = File;
 module.exports.Directory = Directory;
 
 // Save a reference to loaddir for testing loaddir options
-File.prototype.loaddir = Directory.prototype.loaddir = module.exports;
+File.prototype.loaddir = Directory.prototype.loaddir = loaddir;
+
+loaddir.package = packageInfo;
+loaddir.version = packageInfo.version;
+module.exports = loaddir;
